bf3a0a5461e3ee494832727f327c213de62915b3
Mark Tennyson <mark.tennyson@husqvarnaglobal.com>
2023-07-10 18:03:15 +0200

    Lesson 4.3 Moving the world

--- WPFAPP/MainWindow.xaml.cs
+++ WPFAPP/MainWindow.xaml.cs
@@ -31,6 +31,30 @@ namespace WPFAPP
             /// DataContext is a built in proerty for xaml windows
             DataContext = _gameSession;
         }
-        
+
+        private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveNorth();
+        }
+
+        private void OnClick_MoveWest(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveWest();
+        }
+
+        private void OnClick_MoveEast(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveEast();
+        }
+
+        private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveSouth();
+        }
+
+
+
+
+
     }
 }
